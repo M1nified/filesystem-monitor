48e1926f14fe62abc57df584f66142b5cad34a66
M!nified <gora.pub@gmail.com>
2017-07-19 12:59:03 +0200

    comparison selector

--- cli.php
+++ cli.php
@@ -5,11 +5,14 @@ require_once(__DIR__.DIRECTORY_SEPARATOR.'functions.php');
 $will_email = false;
 
 $opts = getopt("", [
-    "dir::",
-    "sum",
     "cmp",
+    "cmp1:",
+    "cmp2:",
     "dbg",
-    "mail-to::"
+    "dir:",
+    "info",
+    "mail-to:",
+    "sum",
     ]);
 
 if (is_array($opts) && array_key_exists('dbg', $opts)) {
@@ -17,13 +20,18 @@ if (is_array($opts) && array_key_exists('dbg', $opts)) {
 }
 
 if (sizeof($opts) == 0) {
+    echo "Usage:\n";
+    echo "php cli.php --dir <dir>[ --cmp[ --cmp1 <file1> --cmp2 <file2>]][ --dbg][ --info][ --mail-to <emails>][ --sum]\n";
     echo "Options:\n";
     $o = [
-        "--dir"     => "Directory to work on. --dir=\"path\"",
-        "--sum"     => "Create sum",
         "--cmp"     => "Compare sums",
+        "--cmp1"    => "Compare file 1; path or file name or its part",
+        "--cmp2"    => "Compare file 2",
         "--dbg"     => "Prints out a debug log",
-        "--mail-to" => "Report recipients. --mail-to=\"john@domain.com,will@domain.com\"",
+        "--dir"     => "Directory to work on. --dir \"path\"",
+        "--info"    => "Displays info about directory",
+        "--mail-to" => "Report recipients. --mail-to \"john@domain.com,will@domain.com\"",
+        "--sum"     => "Create sum",
     ];
     foreach ($o as $option => $description) {
         echo "\t".str_pad($option, 20, " ")."\t".$description."\n";
@@ -41,7 +49,7 @@ $name = md5($dir);
 $mem_dir = __DIR__.DIRECTORY_SEPARATOR."mem-".$name;
 
 $date = date('Y-m-d-H-i-s');
-$hash = str_shuffle('bestcompanyever');
+$hash = str_shuffle(preg_replace('/[\s-]/i', '', 'The Human Spirit Must Prevail Over Technology - Albert Einstein'));
 $fname = "$date-$hash";
 global $LOGG_DST_FILE_PATH;
 $LOGG_DST_FILE_PATH = $mem_dir.DIRECTORY_SEPARATOR."logg-$fname";
@@ -77,6 +85,22 @@ if (is_array($config['exclude'])) {
     }
 }
 
+if (array_key_exists('info', $opts)) {
+    logg_h1("directory info", LoggDestination::LOGG_DST_ALL);
+    $mems = scandir($mem_dir, SCANDIR_SORT_DESCENDING);
+    $sum_files = array_filter($mems, function ($file) {
+        return preg_match('/^sum-.*/i', $file);
+    });
+    $log_files = array_filter($mems, function ($file) {
+        return preg_match('/^logg-.*/i', $file);
+    });
+    logg("Mem dir:     $mem_dir");
+    logg("All files:   ".sizeof($mems));
+    logg("SUM files:   ".sizeof($sum_files));
+    logg("LOG files:   ".sizeof($log_files));
+    logg("OTHER files: ".(sizeof($mems)-sizeof($sum_files)-sizeof($log_files)-2));
+}
+
 if (array_key_exists('sum', $opts)) {
     $sums = sum_dir($dir, ['exclude'=>$config['exclude']]);
     $str = serialize($sums);
@@ -96,8 +120,40 @@ if (array_key_exists('cmp', $opts)) {
     if (sizeof($sum_files) < 2) {
         logg("Nothing to compare.\n", LoggDestination::LOGG_DST_ALL);
     } else {
-        $last_file = $mem_dir.DIRECTORY_SEPARATOR.$sum_files[0];
-        $prev_file = $mem_dir.DIRECTORY_SEPARATOR.$sum_files[1];
+        if (array_key_exists('cmp1', $opts) && array_key_exists('cmp2', $opts)) {
+            $prev_file = realpath($opts['cmp1']);
+            if ($prev_file === false) {
+                $prev_files = array_values(array_filter($sum_files, function ($file) use ($opts) {
+                    return strpos($file, $opts['cmp1']) !== false;
+                }));
+                if (sizeof($prev_files)===1) {
+                    $prev_file = $prev_files[0];
+                } else {
+                    exit("Failed to match cmp1 with any file.");
+                }
+                $prev_file = $mem_dir.DIRECTORY_SEPARATOR.$prev_file;
+            }
+            $last_file = realpath($opts['cmp2']);
+            if ($last_file === false) {
+                $last_files = array_values(array_filter($sum_files, function ($file) use ($opts) {
+                    return strpos($file, $opts['cmp2']) !== false;
+                }));
+                if (sizeof($last_files)===1) {
+                    $last_file = $last_files[0];
+                } else {
+                    exit("Failed to match cmp2 with any file.");
+                }
+                $last_file = $mem_dir.DIRECTORY_SEPARATOR.$last_file;
+            }
+        } else {
+            $last_file = $mem_dir.DIRECTORY_SEPARATOR.$sum_files[0];
+            $prev_file = $mem_dir.DIRECTORY_SEPARATOR.$sum_files[1];
+        }
+
+        logg_h1("File 1:", LoggDestination::LOGG_DST_ALL);
+        logg(basename($prev_file));
+        logg_h1("File 2:", LoggDestination::LOGG_DST_ALL);
+        logg(basename($last_file));
 
         $last = is_array($sums) ? $sums : read_sum($last_file);
         $prev = read_sum($prev_file);
